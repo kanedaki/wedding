0bcf0218f29be6ac6aa5087961e117c0a97c4760
Miguel <kanedaki@gmail.com>
2012-06-07 23:50:33 +0200

    gallery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = '69b88171bd1b4af22312853bed7855787e2a506a62aa66537ae4b27cbd650d0e39154d801c171c0a5505a22c3b53edfdc23ce636d3a2c416971b1675a231b45c'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Wedding::Application.routes.draw do
+  root :to => 'pages#index'
+end
