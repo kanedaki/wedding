cceb017ae588ad9eb4405c66c306c27322d50366
Miguel <kanedaki@gmail.com>
2012-06-15 08:25:03 +0200

    first it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Wedding::Application.routes.draw do
-  root :to => 'pages#index'
+  root :to => 'pages#index', as: :home
+  get '/where' => 'pages#where', as: :where
+  get '/questions' => 'pages#questions', as: :questions
+  get '/suggestions' => 'pages#suggestions', as: :suggestions
 end
