a5e2e8a76546c4d9b5fc07448817c667f504f45d
Miguel <kanedaki@gmail.com>
2012-06-21 23:58:34 +0200

    fixed bug on routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def index
     @photos = Photo.order('random()').all
   end
+
+  def where
+    @photos = Photo.order('random()').all
+  end
+
+  def suggestions
+    @photos = Photo.order('random()').all
+  end
+  
+  def questions
+    @photos = Photo.order('random()').all
+  end
 end
