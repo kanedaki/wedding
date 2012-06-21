1744a5a6b9619862aeba7fef66884a0e0d806aec
Miguel <kanedaki@gmail.com>
2012-06-21 23:51:10 +0200

    fix s3 bucket

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Wedding::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
